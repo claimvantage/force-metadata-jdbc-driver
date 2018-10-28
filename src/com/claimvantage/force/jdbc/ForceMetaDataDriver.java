b1815b3cfab04a89dd23b5cfa18641331583d130
Keith Clarke <keithclarke@claimvantage.com>
2018-10-28 12:56:11 +0000

    Update ForceMetaDataDriver.java

--- src/com/claimvantage/force/jdbc/ForceMetaDataDriver.java
+++ src/com/claimvantage/force/jdbc/ForceMetaDataDriver.java
@@ -7,6 +7,7 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 import java.util.Properties;
 
 /**
